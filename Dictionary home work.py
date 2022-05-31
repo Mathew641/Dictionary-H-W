59f3da2aae597a13859261a3ea492312e7e2d21d
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2022-06-01 00:53:32 +0530

    Add files via upload
    Hello Harry sir 
    I started watching your python tutorial just few days back
    they are great.

---
+++ Dictionary home work.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+dict={"Toilsome":"Arduous", "Elated":"Joyful", "Swashbuckling":"Swaggering, Bold, Dashing", "Endure":"Suffer, Face",
+      "Ignominious":"Humiliating, Discreditable, Disgraceful", "Entice":"tempt, Lure, allure, attract"}
+while(1):
+    a=input("Enter the word: ")
+    b=a.capitalize()
+    print(dict[b])
+
+
+
+
+
+
+
